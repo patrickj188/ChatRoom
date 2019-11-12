e562b6443654e74f21ac9fe2ae847f15d47e86ba
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-11-12 13:18:51 -0600

    add some styles to the sidebar component, started working on dropdown menu for each chat

--- chatroom/src/Components/sideBar/SideBar.js
+++ chatroom/src/Components/sideBar/SideBar.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 
 const SideContainer = styled.div`
-width: 70px;
+width: 10vw;
 height: 100vh;
 border: 1px solid black;
 position: absolute;
@@ -12,22 +12,38 @@ left: 0;
 
 `;
 
+const RoomIcon = styled.button`
+width: 8vw;
+height: 12vh;
+background-color: salmon;
+position: absolute;
+margin: 5px;
+text-align: center;
+border-radius: 15%;
+top: 10%;
+`;
 
-const SideBar = (props) =>{
+
+const SideBar = (props) => {
 
 
-    const renderRoomNames = props.rooms.map( x =>{
+    const renderRoomNames = props.rooms.map(x => {
         return <div>{x.name}</div>
     })
-console.log(props.rooms)
+    console.log(props.rooms)
     return (
         <SideContainer>
-            {renderRoomNames}
+            <RoomIcon>
+                {renderRoomNames}
+            </RoomIcon>
         </SideContainer>
     )
 }
 const mapStateToProps = (state) => {
-    return { userId: state.auth.userId, displayName: state.auth.displayName, isSignedIn: state.auth.isSignedIn, rooms: state.auth.rooms};
-  };
+    return { userId: state.auth.userId, 
+        displayName: state.auth.displayName, 
+        isSignedIn: state.auth.isSignedIn, 
+        rooms: state.auth.rooms };
+};
 
-export default connect(mapStateToProps, { })(SideBar);
+export default connect(mapStateToProps, {})(SideBar);
